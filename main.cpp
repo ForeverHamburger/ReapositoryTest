11c4a453bc799328138c27ebde7e1a66772286aa
littlehamburger <2042347659@qq.com>
2024-03-05 22:42:56 +0800

    v3huitui

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!55555" << std::endl;
+    std::cout << "Hello, World!v33333333" << std::endl;
     return 0;
 }
