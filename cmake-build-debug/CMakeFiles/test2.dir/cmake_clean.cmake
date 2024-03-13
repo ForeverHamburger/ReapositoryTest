38f769f7059f69a39292e8fe90a08f8ff2cfae4d
littlehamburger <2042347659@qq.com>
2024-03-13 20:36:20 +0800

    v6

---
+++ cmake-build-debug/CMakeFiles/test2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test2.dir/main.cpp.o"
+  "CMakeFiles/test2.dir/main.cpp.o.d"
+  "test2"
+  "test2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/littlehamburger/桌面/C_Language_Learning/test2
+# Install script for directory: /home/littlehamburger/桌面/C_Language_Learning/gitlearn/ReapositoryTest
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/littlehamburger/桌面/C_Language_Learning/test2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/littlehamburger/桌面/C_Language_Learning/gitlearn/ReapositoryTest/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
